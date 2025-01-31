54b47a4dd2d775007bb77b5212b5bcfec3e743e9
Volodymyr Obrizan <volodymyr.obrizan@gmail.com>
2025-01-31 18:42:50 +0200

    Autowating for WebElement.clear()

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Welcome to the Selenium + Python Template Project! This repository provides a we
 - **Scalability**: Easy to extend for larger projects.
 - **Integration-Ready**: Built-in support for integrating with APIs, CI/CD pipelines, and reporting tools.
 - **Cross-Browser Support**: Pre-configured WebDriver factory for managing multiple browsers.
-- **Auto-waiting**: `WebDriver.click()` waits for an element to be clickable automatically.
+- **Autowaiting**: `WebElement.click()`, `WebElement.send_keys()`, `WebElement.clear()` waits for an element to be enabled/clickable/editable automatically, use `autowait` fixture. Feature inspired by Playwright.
 
 ## 🏗️ Project Structure
 
--- utils/autowait.py
+++ utils/autowait.py
@@ -1,4 +1,4 @@
-# Monkey-patching WebElement.click to add retry logic for unclickable elements.
+# Monkey-patching WebElement methods to add waiting logic for unclickable elements.
 # This patch is applied globally to all WebElement instances.
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
@@ -45,7 +45,27 @@ def patched_send_keys_factory(timeout: float):
     return patched_send_keys
 
 
-def enable_autowait(timeout: float=10.0) -> None:
+def patched_clear_factory(timeout: float):
+    """
+    Args:
+        timeout (float): Maximum time to wait for the element to become clickable.
+    """
+
+    def patched_clear(self, *args, **kwargs):
+        """
+        Patches the WebElement's clear method to wait for the element to be clickable.
+        Args:
+            self: WebElement object.
+        """
+        driver = self.parent
+        WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(self))
+
+        self._original_clear(*args, **kwargs)
+
+    return patched_clear
+
+
+def enable_autowait(timeout: float = 10.0) -> None:
     """
     Enables auto-waiting globally for all WebDriver.click() invokcations
     Args:
@@ -54,16 +74,22 @@ def enable_autowait(timeout: float=10.0) -> None:
     """
     assert not hasattr(WebElement, "_original_click"), "Autowait already enabled."
     assert not hasattr(WebElement, "_original_send_keys"), "Autowait already enabled."
+    assert not hasattr(WebElement, "_original_clear"), "Autowait already enabled."
     WebElement._original_click = WebElement.click
     WebElement.click = patched_click_factory(timeout)
     WebElement._original_send_keys = WebElement.send_keys
     WebElement.send_keys = patched_send_keys_factory(timeout)
+    WebElement._original_clear = WebElement.clear
+    WebElement.clear = patched_clear_factory(timeout)
 
 
 def disable_autowait() -> None:
     assert hasattr(WebElement, "_original_click"), "Autowait is not enabled."
     assert hasattr(WebElement, "_original_send_keys"), "Autowait is not enabled."
+    assert hasattr(WebElement, "_original_clear"), "Autowait is not enabled."
     WebElement.click = WebElement._original_click
-    del(WebElement._original_click)
+    del WebElement._original_click
     WebElement.send_keys = WebElement._original_send_keys
-    del(WebElement._original_send_keys)
+    del WebElement._original_send_keys
+    WebElement.clear = WebElement._original_clear
+    del WebElement._original_clear
